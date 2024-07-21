d4f9f734019d48ce43264bce7234aa5150507bb8
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-07-21 00:26:05 -0300

    chore return data instead of response

--- functions/lib/mandabem/create-tag.js
+++ functions/lib/mandabem/create-tag.js
@@ -86,7 +86,7 @@ module.exports = (apiId, apiKey, order, refId) => {
                   auth
                 ))
               }
-              return response
+              return data
             }).catch(console.error))
         }
       }
